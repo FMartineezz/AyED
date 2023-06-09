4f61b93347ed1ae4db94018b9e0bc5dde5bd3a1b
Fmartinez <facmartinez@frba.utn.edu.ar>
2023-06-09 20:58:42 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -2,24 +2,24 @@
 #include <iomanip>
 using namespace std;
 
-int main ()
-
+int main()
 {
-long long int iteraciiones {1000000};
-double pi {0};
-double numerador {1};
-double denominador {1};
-double cambiosig {1};
-int numiteracion {0};
+    double precision = 0.0000001;
+    double pi {0};
+    double numerador {1};
+    double denominador {1};
+    double cambiosig {1};
+    double aprox {0};
 
-while (numiteracion < iteraciiones)
-{
-   pi +=  cambiosig * (numerador / denominador); 
-   denominador += 2; 
-   cambiosig *= -1;
-   ++numiteracion;
-}
-cout << fixed << setprecision(6);
-cout << pi * 4 << endl;
-return 0;
+    do
+    {
+        aprox = pi;
+        pi += cambiosig * (numerador / denominador);
+        denominador += 2;
+        cambiosig *= -1;
+    } while ((abs(pi - aprox) ) > precision);
+
+    cout << fixed << setprecision(6);
+    cout << pi * 4 << endl;
+    return 0;
 }
